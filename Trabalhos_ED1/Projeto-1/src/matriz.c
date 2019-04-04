a064b9683117d27dd2a243c4c15b1de7b90d9687
Lukas Dias <diaslukas19@gmail.com>
2019-04-04 12:19:21 -0300

    f-commit

--- Trabalhos_ED1/Projeto-1/src/matriz.c
+++ Trabalhos_ED1/Projeto-1/src/matriz.c
@@ -13,6 +13,7 @@ void LerMa() {
     arqMA = fopen("docs/matriz_a.txt","r");
     arqMB = fopen("docs/matriz_b.txt","r");
 
+    //Testa Arquivo//
     if (arqMA == NULL) {
        printf("Erro: Nao foi possivel abrir o arquivo da matriz A!!!\n");
        fclose(arqMA);
@@ -22,10 +23,12 @@ void LerMa() {
        printf("Erro: Nao foi possivel abrir o arquivo da matriz B!!!\n");
        fclose(arqMB);
     } 
+    /////////////// 
 
-    //Lê primeira linha do arquivo(MA) obtem o numero de linhas e colunas da matriz//
+    //Lê primeira linha 0 do arquivo(MA) obtem o numero de linhas e colunas da matriz//
     fscanf(arqMA, "%d %d", &linhasA ,&colunasA);
     Vma = AlocVMat(linhasA, colunasA);
+    //Lê matriz A completa//
     for (i = 0;i<linhasA;i++){
         for(j = 0;j<colunasA;j++){
             fscanf(arqMA,"%f ", &Vma[i*colunasA+j]);
@@ -33,9 +36,10 @@ void LerMa() {
     }
     fclose(arqMA);
 
-    //Lê primeira linha do arquivo(MB) obtem o numero de linhas e colunas da matriz//
+    //Lê primeira linha 0 do arquivo(MB) obtem o numero de linhas e colunas da matriz//
     fscanf(arqMB, "%d %d", &linhasB ,&colunasB);
     Vmb = AlocVMat(linhasB, colunasB);
+    //Lê matriz B completa//
     for (i = 0;i<linhasB;i++){
         for(j = 0;j<colunasB;j++){
             fscanf(arqMB,"%f ", &Vmb[i*colunasB+j]);
@@ -43,13 +47,17 @@ void LerMa() {
     }
     fclose(arqMB);
 
+    //Menu principal recebendo os devidos parametros//
     menu(linhasA, colunasA, linhasB, colunasB, Vma, Vmb);
 
+    //Liberar memória alocada//
     FreeMat(Vma);
     FreeMat(Vmb);
           
 }
 
+//FUNÇÃO MENU RECEBE LINHAS A/COLUNAS DE A, LINHAS DE B/COLUNAS DE B
+//E AS MATRIZES A E B PARA SEREM PROCESSADAS NAS OUTRAS FUNÇÕES//
 void menu(int n, int m, int p, int q, float *vmA, float *vmB) {
     int condit = 1, k;
     while (condit!=0) {
@@ -114,6 +122,7 @@ void menu(int n, int m, int p, int q, float *vmA, float *vmB) {
     }
 }
 
+//FUNÇÃO PARA PRINTAR AS MATRIZES TODAS VEZ QUE ALGUMA OPERAÇÃO FOR FEITA COM ELAS//
 void printAB(int n, int m, int p, int q, float *ma, float *mb){
     int i, j;
     
@@ -125,7 +134,7 @@ void printAB(int n, int m, int p, int q, float *ma, float *mb){
         }
         putchar('\n');
     }
-
+    printf("===============Matriz A=================\n\n");
     //Print Matriz B//
     printf("===============Matriz B=================\n");
     for(i=0;i<q;i++){
@@ -134,10 +143,12 @@ void printAB(int n, int m, int p, int q, float *ma, float *mb){
         }
         putchar('\n');
     }
-
-    system("PAUSE");
+    printf("===============Matriz B=================\n");
+    putchar('\n');    
 }
 
+//FUNÇÃO PARA ALOCAR UMA MATRIZ DINAMICAMENTE DENTRO DE UM VETOR//
+//RETORNA UMA MATRIZ ALOCADA;
 float *AlocVMat(int linhas, int colunas) {
    float *vmat;
    if ((linhas < 1) || (colunas < 1)) return NULL;
@@ -149,6 +160,9 @@ float *AlocVMat(int linhas, int colunas) {
    return vmat;
 }
 
+//FUNÇÃO QUE IMPRIME O PRODUTO DA MATRIZ A X B, TESTANDO SE AS COLUNAS DE A == LINHAS DE B//
+//RETORNA 0 SE A OPERAÇÃO NÃO FOR POSSIVEL//
+//RETORNA 1 SE A OPERAÇÃO FOI UM SUCESSO//
 int ProdAxB(int n, int m, int p, int q, float *ma, float *mb){
     int i, j, k;
     float *mc;
@@ -171,15 +185,20 @@ int ProdAxB(int n, int m, int p, int q, float *ma, float *mb){
             }
             putchar('\n');
         }
+        printf("==========Matriz Produto AXB===========\n");
+        putchar('\n');
         system("PAUSE");
         free(mc);
         return 0;
     }
-    printf("\tA multiplicacao nao e possivel pois, colunas de A != linhas de B\n");
+    printf("A multiplicacao nao e possivel pois, colunas de A != linhas de B\n");
     system("PAUSE");
     return 0;
 }
 
+//FUNÇÃO QUE IMPRIME O PRODUTO DA MATRIZ A X B(TRANSPOSTA), TESTANDO SE AS COLUNAS DE A == LINHAS DE B E SE É POSSÍVEL TRANSPOR B//
+//RETORNA 0 SE A OPERAÇÃO NÃO FOR POSSIVEL//
+//RETORNA 1 SE A OPERAÇÃO FOI UM SUCESSO//
 int ProdAxBT(int n, int m, int p, int q, float *ma, float *mb){
     int i, j, k;
     float *mbT = Transposta(p, q, mb);
@@ -188,152 +207,147 @@ int ProdAxBT(int n, int m, int p, int q, float *ma, float *mb){
 
     if(m==p){
         mc = AlocVMat(n, q);
-        for(i=0;i<n;i++){
-            for(j=0;j<q;j++){
-                for(k=0;k<n;k++){
-                    sum += ma[i*q+k] * mbT[k*q+j];
+        if(mc!=NULL){
+            for(i=0;i<n;i++){
+                for(j=0;j<q;j++){
+                    for(k=0;k<n;k++){
+                        sum += ma[i*q+k] * mbT[k*q+j];
+                    }
+                    mc[i*q+j] = sum;
+                    sum = 0;
                 }
-                 mc[i*q+j] = sum;
-                 sum = 0;
             }
-        }
-        printf("==========Matriz Produto AXBT===========\n");
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                printf("%.2f ", mc[i*m+j]);
+            printf("==========Matriz Produto AXBT===========\n");
+            for(i=0;i<n;i++){
+                for(j=0;j<m;j++){
+                    printf("%.2f ", mc[i*m+j]);
+                }
+                putchar('\n');
             }
+            printf("==========Matriz Produto AXBT===========\n");
+            system("PAUSE");
             putchar('\n');
+            free(mbT);
+            free(mc);
+            return 1;
         }
-
-        system("PAUSE");
-        free(mbT);
         free(mc);
-        return 0;
     }
+
     printf("\tA multiplicacao nao e possivel pois, colunas de A != linhas de B\n");
     system("PAUSE");
     free(mbT);
     return 0;
 }
 
+//TRANSPÕE E RETORNA A MATRIZ RECEBIDA COMO PARAMETRO(float *ma)//
 float *Transposta(int n, int m, float *ma){
    float *maT;
    int i, j;
-   if (ma!=NULL && n == m){
+   if (ma!=NULL){
       maT = (float *) malloc(n*m * sizeof(float));
-      if (maT!=NULL){
-         for(i = 0;i<n;i++){
-            for(j=0;j<m;j++){
-               maT[i*m+j] = ma[j*m+i];
-            }
-         }
-         printf("==========Matriz Transposta===========\n");
-         for(i = 0;i<n;i++){
-            for(j=0;j<m;j++){
-               printf("%.2f ", maT[i*m+j]);
+        if (maT!=NULL) {
+            for(i = 0;i<n;i++){
+                for(j=0;j<m;j++){
+                    maT[i*m+j] = ma[j*m+i];
+                }
             }
             putchar('\n');
-         }
-         putchar('\n');
-         system("PAUSE");
-         return maT;
-      }
+
+            printf("==========Matriz Transposta===========\n");
+            for(i = 0;i<n;i++){
+                for(j=0;j<m;j++){
+                    printf("%.2f ", maT[i*m+j]);
+                }
+                putchar('\n');
+            }
+            putchar('\n');
+            system("PAUSE");
+            putchar('\n');
+            return maT;
+        }
    }
+
    printf("Nao foi possivel transpor a matriz, pois o numero de linhas != colunas!!!\n");
    return NULL;
    
 }
 
+//IMPRIME NA TELA OS ELEMENTOS DA TRIANGULAR SUP DE A, OU SEJA, TODOS ELEM ONDE J>I//
 int TriangularSup(int n, float *vma){
-   int i, j, k = 0, cont = 0;
-   float *vT;
-   if(vma!=NULL){
-      for(i=0;i<n;i++){
-         for(j=0;j<n;j++){
-            if(j > i){
-               cont++;
-            }
-         }
-      }
-      vT = (float *) malloc (cont*sizeof(float));
-      for(i=0;k<cont;i++){
-         for(j=0;j<n;j++){
-            if(j>i){
-               vT[k] = vma[i*n+j];
-               k++;
-            }
-         }
-      }
-      for(i=0;i<cont;i++){
-         printf("%.2f[%d]\n", vT[i], i);
-      }
-      system("PAUSE");
-      FreeMat(vT);
-      return 1;
-   }
-   return 0;
-}
-
-int DiagonalP(int n, int m, float *ma){
-    int i, j;
-   if((ma == NULL)){
-      return 0;
-   }
-
-   float *v;
-   v = (float *) malloc(m*sizeof(float));
-   if(v != NULL){
-      for(i = 0; i<n;i++){
-        for(j = 0; j<m;j++) {
-            if(i==j) {
-                v[i] = ma[i*m+j];
-            }
-        }
-      }
-
-      for(i=0;i<m;i++){
-          printf("%.2f[%d]\n", v[i], i);
-      }
-      system("PAUSE");
-      FreeMat(v);
-      return 1;
-   }
-   return 0;
-
-}
-
-int RetornaLinha(int n, int m, float *vma, int k){
-    int i, j;
-   if(vma != NULL && n>0 && m>0 && n >= k){
-        for(i = 0; i<n ;i++){
-            for(j = 0; j<m ;j++){
-                if (i == k){
-                    printf("%.2f\n", vma[i*m+j]);
+   int i, j;
+    if(vma!=NULL){
+        printf("=======ELEMENTOS DA TRIANGULAR de A=======\n");
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                if(j>i){
+                    printf("%.2f[%d][%d]\n", vma[i*n+j], i, j);
                 }
             }
         }
         system("PAUSE");
         return 1;
-   }
+    }
+   return 0;
+}
+
+//IMPRIME NA TELA OS ELEMENTOS DA DIAGONAL PRINCIPAL DE A, OU SEJA, TODOS ELEM ONDE I == J//
+int DiagonalP(int n, int m, float *ma){
+    int i, j;
+    if(ma!=NULL && n==m){
+        //Recebendo elementos da Diagonal  e armazenando num array de tamanho [m]//
+        printf("=======ELEMENTOS DA DIAGONAL P. de A=======\n");
+        for(i = 0; i<n;i++){
+            for(j = 0; j<m;j++) {
+                if(i==j) {
+                    printf("%.2f[%d][%d]\n", ma[i*m+j], i, j);
+                }
+            }
+        }
+        system("PAUSE");
+        return 1;
+    }
+    printf("ERRO, function DiagonalP()\nMatriz N e QUADRADA!!!\n");
+    return 0;
+
+}
+
+//IMPRIME NA TELA TODOS OS ELEMENTOS DA LINHA REQUISITADA PELO USER USANDO UMA ENTRADA DO TECLADO PASSA PARA A VARIAVEL K//
+int RetornaLinha(int n, int m, float *vma, int k){
+    int i, j;
+    if(vma != NULL && n>0 && m>0 && n >= k){
+        for(i = 0; i<n ;i++) {
+            for(j = 0; j<m ;j++) {
+                if (i == k) {
+                    printf("%.2f[%d][%d]\n", vma[i*m+j], i, j);
+                }
+            }
+        }
+        system("PAUSE");
+        return 1;
+    }
     return 0;
 }
 
+//IMPRIME NA TELA TODOS OS ELEMENTOS DA COLUNA REQUISITADA PELO USER USANDO UMA ENTRADA DO TECLADO PASSA PARA A VARIAVEL K//
 int RetornaColuna(int n, int m, float *vma, int k){
     int i, j;
-   if(vma != NULL && n>0 && m>0 && n >= k){
+    if(vma != NULL && n>0 && m>0 && n >= k){
         for(i = 0; i<n ;i++){
             for(j = 0; j<m ;j++){
                 if (j == k){
-                    printf("%.2f\n", vma[i*m+j]);
+                    printf("%.2f[%d][%d]\n", vma[i*m+j], i, j);
                 }
             }
         }
         system("PAUSE");
         return 1;
-   }
+    }
+
     return 0;
 }
 
+//LIBERA MEMORIA ALOCADA PELAS MATRIZES//
 float *FreeMat(float *vmat) {
    free(vmat);
    printf("Liberou\n");
