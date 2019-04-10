6791cc623fb7029a4e493b7a2ded8f2f7fc38084
Lukas Dias <diaslukas19@gmail.com>
2019-04-10 14:00:29 -0300

    .h start

--- Trabalhos_ED1/Projeto-2/header/cofo.h
+++ Trabalhos_ED1/Projeto-2/header/cofo.h
@@ -1,3 +1,19 @@
+#ifndef cofo_h
+#define cofo_h
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct _cofo_{
+    int max;
+    int nElms;
+    void **elms;
+}Cofo;
+
+    #ifndef cofo_c
+        Cofo *novoCofo(int max);
+    #else    
+    #endif
+    
+#endif
+
+
